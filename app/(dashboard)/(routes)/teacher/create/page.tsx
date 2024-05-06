afabbbe79247bcfe8454b8cf5f85120d07991b21
dandamudi2 <srinivasd2mail@gmail.com>
2024-05-06 14:56:35 +0800

    course creation

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -45,8 +45,11 @@ const CreatePage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post("/api/course", values);
+      console.log(values);
+      const response = await axios.post("/api/courses", values);
+
       router.push(`/teacher/courses/${response.data.id}`);
+      toast.success("Course cretaed!");
     } catch {
       toast.error("Something went wrong!");
     }
